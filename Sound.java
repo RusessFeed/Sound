5a5fa6062cf8445f814ce46ea149b0fcfee1dbe5
Ruslan <ruslanmarlinskiy@gmail.com>
2024-05-11 00:05:00 +0300

    Add files via upload

---
+++ Sound.java
@@ -0,0 +1,9 @@
+public class Sound {
+    public static void main(String[] args) {
+        double metersToLightning = 7.2 * 335;
+        System.out.println("Расстояние в метрах - " + (int) metersToLightning);
+
+        double kilometersToLightning = metersToLightning / 1000;
+        System.out.println("Расстояние в километрах - " + kilometersToLightning);
+    }
+}
